737e780b46522d49c5cab4a342c4e24a07287949
Gabriel Rodrigues <contatogabrielcruzrodrigues@gmail.com>
2025-04-02 08:11:24 -0300

    fix: environment variables

--- frontend/src/app/environments/environment.ts
+++ frontend/src/app/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
      production: false,
-     apiUrl: 'http://localhost:5171/api',
-     sinalRBackendUrl: 'http://localhost:5171/callHub',
+     apiUrl: 'http://192.168.1.65:5171/api',
+     sinalRBackendUrl: 'http://192.168.1.65:5171/callHub',
      featureFlag: true,
 };
