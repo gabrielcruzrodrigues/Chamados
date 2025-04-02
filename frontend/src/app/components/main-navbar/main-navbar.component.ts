b359190046fe30f0a66cc7cc36e8ab4bdcf8883b
Gabriel Rodrigues <contatogabrielcruzrodrigues@gmail.com>
2025-04-02 07:51:31 -0300

    fix: Resolving navbar show options before render bug

--- frontend/src/app/components/main-navbar/main-navbar.component.ts
+++ frontend/src/app/components/main-navbar/main-navbar.component.ts
@@ -3,31 +3,39 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 
 @Component({
-    selector: 'app-main-navbar',
-    imports: [
-        CommonModule
-    ],
-    templateUrl: './main-navbar.component.html',
-    styleUrl: './main-navbar.component.sass'
+  selector: 'app-main-navbar',
+  imports: [
+    CommonModule
+  ],
+  templateUrl: './main-navbar.component.html',
+  styleUrl: './main-navbar.component.sass'
 })
 export class MainNavbarComponent implements OnInit {
   admin: boolean = false;
   moderador: boolean = false;
-  user: boolean = true;
+  user: boolean = false;
 
   constructor(
     private authService: AuthService
-  ) {}
+  ) { }
 
   async ngOnInit(): Promise<void> {
     const role = await this.authService.getRole();
-    switch(role) {
-      case 0: 
+    switch (role) {
+      case 0:
         this.admin = true;
         this.moderador = true;
+        this.user = true;
+        break;
+      case 1:
+        this.admin = false;
+        this.moderador = false;
+        this.user = true;
         break;
       case 2:
         this.moderador = true;
+        this.admin = false;
+        this.user = true;
         break;
     }
   }
