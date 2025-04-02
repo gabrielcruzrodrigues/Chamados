51935dcd381871a610d205ec9f66e3698ed5d685
Gabriel Rodrigues <contatogabrielcruzrodrigues@gmail.com>
2025-04-02 08:08:12 -0300

    console

--- frontend/src/app/components/main-navbar/main-navbar.component.ts
+++ frontend/src/app/components/main-navbar/main-navbar.component.ts
@@ -14,14 +14,16 @@ export class MainNavbarComponent implements OnInit {
   admin: boolean = false;
   moderador: boolean = false;
   user: boolean = false;
+  userRole: number = 3;
 
   constructor(
     private authService: AuthService
   ) { }
 
   async ngOnInit(): Promise<void> {
-    const role = await this.authService.getRole();
-    switch (role) {
+    console.log("user role: " + this.userRole);
+    this.userRole = await this.authService.getRole();
+    switch (this.userRole) {
       case 0:
         this.admin = true;
         this.moderador = true;
@@ -33,8 +35,8 @@ export class MainNavbarComponent implements OnInit {
         this.user = true;
         break;
       case 2:
-        this.moderador = true;
         this.admin = false;
+        this.moderador = true;
         this.user = true;
         break;
     }
